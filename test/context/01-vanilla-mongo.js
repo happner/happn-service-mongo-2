6af05ac8aa4dd4c5bd2bc8881a01ac62dc68ad6a
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2016-08-24 10:00:15 +0200

    vanilla tests passing

--- test/context/01-vanilla-mongo.js
+++ test/context/01-vanilla-mongo.js
@@ -15,14 +15,14 @@ module.exports = {
     var config =  {
       plugin: happn.client_plugins.intra_process,
       context: happnInstance
-    }
+    };
     happn_client.create(config, callback);
   },
   listenerClient:function(happnInstance, callback){
     var config =  {
       plugin: happn.client_plugins.intra_process,
       context: happnInstance
-    }
+    };
     happn_client.create(config, callback);
   }
-}
+};
--- test/happn-test.js
+++ test/happn-test.js
@@ -5,14 +5,22 @@ var serviceInstance = new service();
 
 var config = {
 	url:'mongodb://127.0.0.1:27017/happn'
-}
+};
 
 serviceInstance.initialize(config, function(e){
 
 	if (e) throw e;
 
 	TEST_GLOBALS.mongoService = serviceInstance;
-	var happn_tests = require('happn-tests').instantiate(__dirname + '/context');
+
+	var testConfig = {
+		contextPath:__dirname + '/context',
+		timeout:5000,
+		noBenchmarket:true
+	};
+
+	var happn_tests = require('happn-tests').instantiate(testConfig);
+
 	happn_tests.run(function(e){
 		if (e) {
 			console.log('tests ran with failures', e);
